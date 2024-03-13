40236882910a7ac8b8d601374b15759e4cbcd026
Emilio Silva Schlenker <e18r@disroot.org>
2024-03-13 14:20:09 -0500

    feat: contentEditable

--- pal.js
+++ pal.js
@@ -73,45 +73,40 @@ const integrate = () => {
 };
 
 const type = e => {
-  placeholder.innerHTML = sanitize(stem.value);
-  end.innerHTML = palindromize(normalize(stem.value));
+  suggest.innerHTML = palindromize(normalize(stem.innerText));
 };
 
 const press = e => {
-  if (
-    e.key === "Enter"
-      || (e.key === "ArrowRight" && stem.selectionStart === stem.value.length)
-  ) integrate();
-  else if (e.key === "Backspace" || e.key === "Delete") {
-    const nextLength = stem.value.length - 1;
-    placeholder.innerHTML = sanitize(stem.value.substring(0, nextLength));
-  }
-  else if (e.key.length === 1 && !e.ctrlKey && !e.altKey && !e.metaKey) {
-    placeholder.innerHTML = sanitize(stem.value + e.key);
-  }
+  // if (
+  //   e.key === "Enter"
+  //     || (e.key === "ArrowRight" && stem.selectionStart === stem.value.length)
+  // ) integrate();
+  // else if (e.key === "Backspace" || e.key === "Delete") {
+  //   const nextLength = stem.value.length - 1;
+  //   placeholder.innerHTML = sanitize(stem.value.substring(0, nextLength));
+  // }
+  // else if (e.key.length === 1 && !e.ctrlKey && !e.altKey && !e.metaKey) {
+  //   placeholder.innerHTML = sanitize(stem.value + e.key);
+  // }
 };
 
 document.body.style.margin = 0;
 
 const canvas = document.createElement("div");
-canvas.style.position = "relative";
 canvas.style.margin = "4rem";
+canvas.style.fontSize = "3rem";
+canvas.style.fontFamily = "serif";
+canvas.style.wordBreak = "break-all";
 document.body.append(canvas);
 
-const stem = document.createElement("input");
+const stem = document.createElement("span");
+stem.contentEditable = "plaintext-only";
 stem.setAttribute("autofocus", "autofocus");
 stem.setAttribute("autocomplete", "off");
 stem.setAttribute("autocorrect", "off");
 stem.setAttribute("autocapitalize", "off");
 stem.setAttribute("spellcheck", "false");
-stem.style.position = "relative";
-stem.style.zIndex = 1;
-stem.style.width = "100%";
-stem.style.borderStyle = "none";
 stem.style.outline = "none";
-stem.style.padding = 0;
-stem.style.fontSize = "3rem";
-stem.style.fontFamily = "serif";
 stem.style.caretColor = "red";
 stem.onkeydown = press;
 stem.onkeyup = type;
@@ -119,29 +114,12 @@ canvas.append(stem);
 window.onmouseover = () => stem.focus();
 window.onclick = () => stem.focus();
 
-const endContainer = document.createElement("div");
-endContainer.style.position = "absolute";
-endContainer.style.top = 0;
-endContainer.style.fontSize = "3rem";
-endContainer.style.fontFamily = "serif";
-canvas.append(endContainer);
-
-const placeholder = document.createElement("span");
-placeholder.style.position = "relative";
-placeholder.style.zIndex = 0;
-endContainer.append(placeholder);
-
-const end = document.createElement("span");
-end.style.position = "relative";
-end.style.zIndex = 2;
-end.style.color = "#A0A0A0";
-end.onclick = () => integrate();
-endContainer.append(end);
+const suggest = document.createElement("span");
+suggest.style.color = "gray";
+canvas.append(suggest);
 
 if (window.location.search === "?dev") {
   canvas.style.border = "2px dotted red";
   stem.style.border = "3px dotted gray";
-  endContainer.style.border = "2px dashed blue";
-  placeholder.style.border = "2px outset yellow";
-  end.style.border = "2px groove green";
+  suggest.style.border = "2px dashed pink";
 }
