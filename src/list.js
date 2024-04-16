22e6d8038a28fcb952c7c42cb537b035cd693328
Emilio Silva Schlenker <e18r@disroot.org>
2024-04-16 18:37:16 -0500

    refactor(list): move list-related code to its own file

---
+++ src/list.js
@@ -0,0 +1,103 @@
+const LOADING_CARD_TEXT = "Se dice: de ti seré si te decides";
+const LOADING_CARD_AMOUNT = 5;
+
+const loadingCards = [];
+const cardIds = [];
+
+const createCard = () => {
+  const card = document.createElement("div");
+  card.style.borderStyle = "solid";
+  card.style.borderWidth = "1px";
+  card.style.borderColor = "black";
+  card.style.height = "4rem";
+  card.style.fontSize = "2rem";
+  card.style.display = "flex";
+  card.style.alignContent = "center";
+  card.style.justifyContent = "center";
+  card.style.flexWrap = "wrap";
+  card.style.overflowX = "auto";
+  return card;
+};
+
+const setLoading = card => {
+  card.className = "loading";
+  card.innerText = LOADING_CARD_TEXT;
+  card.style.color = "transparent";
+  card.style.userSelect = "none";
+  card.animate(
+    {
+      textShadow: ["0 0 10px black", "0 0 10px gray", "0 0 10px black"],
+      easing: "ease-in"
+    },
+    {
+      duration: 1500,
+      iterations: Infinity
+    }
+  );
+};
+
+const populate = (card, text) => {
+  card.className = "populated";
+  card.innerText = text;
+  card.style.textShadow = "initial";
+  card.style.color = "black";
+  card.style.userSelect = "initial";
+  card.getAnimations().forEach(animation => animation.cancel());
+};
+
+const setText = (card, text) => {
+  card.innerText = text;
+};
+
+const addLoadingCards = () => {
+  for (let i = 0; i < LOADING_CARD_AMOUNT; i++) {
+    const card = createCard();
+    setLoading(card);
+    list.prepend(card);
+    loadingCards.unshift(card);
+  }
+};
+
+const addCard = (text, id) => {
+  if (cardIds.includes(id)) return;
+  else cardIds.push(id);
+  if (loadingCards.length) {
+    const card = loadingCards.pop();
+    populate(card, text);
+  } else {
+    const card = createCard();
+    setText(card, text);
+    list.prepend(card);
+  }
+};
+
+const finishLoading = () => {
+  if (loadingCards.length) {
+    loadingCards.forEach(card => card.remove());
+    loadingCards.splice(0, loadingCards.length);
+  }
+};
+
+const getCards = async () => {
+  if (!indr.isOnline()) return;
+  let response;
+  try {
+    const lastCardId = cardIds[cardIds.length - 1];
+    response = await fetch(indr.indr + "/list?after=" + lastCardId);
+  } catch (err) {
+    indr.networkIssue();
+    finishLoading();
+    return;
+  }
+  const palindromes = await response.json();
+  if (palindromes.length === 0) {
+    finishLoading();
+    return;
+  }
+  palindromes.forEach(palindrome => {
+    addCard(palindrome["text"], palindrome["id"]);
+  });
+  finishLoading();
+};
+
+const list = document.createElement("div");
--- src/pal.js
+++ src/pal.js
@@ -26,12 +26,7 @@ const ascii = {
 
 const selection = window.getSelection();
 
-const LOADING_CARD_TEXT = "Se dice: de ti seré si te decides";
-const LOADING_CARD_AMOUNT = 5;
-
 let lastCaret = 0;
-const loadingCards = [];
-const cardIds = [];
 
 const isPalindrome = text => {
   return text.split("").reverse().join("") === text;
@@ -100,102 +95,6 @@ const normalize = text => {
   map[normArray.length] = textArray.length;
   const norm = normArray.join("");
   return {norm, map};
-};
-
-const createCard = () => {
-  const card = document.createElement("div");
-  card.style.borderStyle = "solid";
-  card.style.borderWidth = "1px";
-  card.style.borderColor = "black";
-  card.style.height = "4rem";
-  card.style.fontSize = "2rem";
-  card.style.display = "flex";
-  card.style.alignContent = "center";
-  card.style.justifyContent = "center";
-  card.style.flexWrap = "wrap";
-  card.style.overflowX = "auto";
-  return card;
-};
-
-const setLoading = card => {
-  card.className = "loading";
-  card.innerText = LOADING_CARD_TEXT;
-  card.style.color = "transparent";
-  card.style.userSelect = "none";
-  card.animate(
-    {
-      textShadow: ["0 0 10px black", "0 0 10px gray", "0 0 10px black"],
-      easing: "ease-in"
-    },
-    {
-      duration: 1500,
-      iterations: Infinity
-    }
-  );
-};
-
-const populate = (card, text) => {
-  card.className = "populated";
-  card.innerText = text;
-  card.style.textShadow = "initial";
-  card.style.color = "black";
-  card.style.userSelect = "initial";
-  card.getAnimations().forEach(animation => animation.cancel());
-};
-
-const setText = (card, text) => {
-  card.innerText = text;
-};
-
-const addLoadingCards = () => {
-  for (let i = 0; i < LOADING_CARD_AMOUNT; i++) {
-    const card = createCard();
-    setLoading(card);
-    list.prepend(card);
-    loadingCards.unshift(card);
-  }
-};
-
-const addCard = (text, id) => {
-  if (cardIds.includes(id)) return;
-  else cardIds.push(id);
-  if (loadingCards.length) {
-    const card = loadingCards.pop();
-    populate(card, text);
-  } else {
-    const card = createCard();
-    setText(card, text);
-    list.prepend(card);
-  }
-};
-
-const finishLoading = () => {
-  if (loadingCards.length) {
-    loadingCards.forEach(card => card.remove());
-    loadingCards.splice(0, loadingCards.length);
-  }
-};
-
-const getCards = async () => {
-  if (!indr.isOnline()) return;
-  let response;
-  try {
-    const lastCardId = cardIds[cardIds.length - 1];
-    response = await fetch(indr.indr + "/list?after=" + lastCardId);
-  } catch (err) {
-    indr.networkIssue();
-    finishLoading();
-    return;
-  }
-  const palindromes = await response.json();
-  if (palindromes.length === 0) {
-    finishLoading();
-    return;
-  }
-  palindromes.forEach(palindrome => {
-    addCard(palindrome["text"], palindrome["id"]);
-  });
-  finishLoading();
 };
 
 const publishPalindrome = async () => {
@@ -433,8 +332,6 @@ canvas.append(highlight);
 canvas.append(input);
 canvas.append(angel);
 canvas.append(tailNode);
-
-const list = document.createElement("div");
 
 document.body.style.margin = 0;
 document.body.style.flex = 1;
