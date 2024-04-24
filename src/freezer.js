34d72f3f39a876c8e6e2d871a55d8f2159ef8ad4
Emilio Silva Schlenker <e18r@disroot.org>
2024-04-24 09:46:26 -0500

    feat(freeze): border color instead of text color

--- src/freezer.js
+++ src/freezer.js
@@ -4,14 +4,18 @@ const THAW_CLICKED = new Event("thawClicked");
 
 const erase = () => {
   preNode.innerText = "";
-  preHigh.innerText = "";
+  preNode.style.borderColor = "transparent";
   postNode.innerText = "";
+  postNode.style.borderColor = "transparent";
+  preHigh.innerText = "";
   postHigh.innerText = "";
 };
 
 const add = (start, end) => {
   preNode.innerText += start;
+  preNode.style.borderColor = palette.frozen;
   postNode.innerText = end + postNode.innerText;
+  postNode.style.borderColor = palette.frozen;
   preHigh.innerText = preNode.innerText;
   postHigh.innerText = postNode.innerText;
 };
@@ -22,19 +26,29 @@ const post = () => postNode.innerText;
 
 const preHigh = document.createElement("span");
 preHigh.id = "preHigh";
+preHigh.style.borderStyle = "solid none solid solid";
+preHigh.style.borderWidth = "0.3rem";
+preHigh.style.borderColor = "transparent";
 
 const postHigh = document.createElement("span");
 postHigh.id = "postHigh";
+postHigh.style.borderStyle = "solid solid solid none";
+postHigh.style.borderWidth = "0.3rem";
+postHigh.style.borderColor = "transparent";
 
 const preNode = document.createElement("span");
 preNode.id = "preNode";
-preNode.style.color = palette.frozen;
+preNode.style.borderStyle = "solid none solid solid";
+preNode.style.borderWidth = "0.3rem";
+preNode.style.borderColor = "transparent";
 preNode.style.cursor = "pointer";
 preNode.onclick = () => document.dispatchEvent(THAW_CLICKED);
 
 const postNode = document.createElement("span");
 postNode.id = "postNode";
-postNode.style.color = palette.frozen;
+postNode.style.borderStyle = "solid solid solid none";
+postNode.style.borderWidth = "0.3rem";
+postNode.style.borderColor = "transparent";
 postNode.style.cursor = "pointer";
 postNode.onclick = () => document.dispatchEvent(THAW_CLICKED);
 
