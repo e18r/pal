05aa3e5f034814bbe9c8566924d236cde6fa02ce
Emilio Silva Schlenker <e18r@disroot.org>
2024-04-23 21:34:37 -0500

    devops: change webpack config into ES6

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const path = require("path");
-const webpack = require("webpack");
-const fs = require("fs");
+import path from "path";
+import webpack from "webpack";
+import fs from "fs";
 
-module.exports = () => {
+export default () => {
   const env = process.env["ENV"];
   const options = {encoding: "utf-8"};
   const data = fs.readFileSync("./settings.json", options);
   const settings = JSON.parse(data);
   const { indrURL } = settings[env];
+  const __dirname = import.meta.dirname;
   console.log("indr URL: ", indrURL, "\n");
   return {
     entry: "./src/pal.js",
